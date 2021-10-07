2451c8c5c8d964a549b893beec3cc96e9342b6c3
Bartosz Wiecheć <32521016+bwiechec@users.noreply.github.com>
2021-10-07 17:56:20 +0200

    Delete Semester 3/OOP/src directory

--- Semester 3/OOP/src/Opcje.java
+++
@@ -1,7 +0,0 @@
-//import java.util.Scanner;
-
-public class Opcje {
-	public void dzialanieNaWpisie(DaneOsobowe osobowe, Adres adres) {
-		System.out.println("Rozpoczynam dzialanie na wpisie.");
-	}
-}
